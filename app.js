b840aacff23e2e5406fc08979ba06c34eb5e013c
Umer Farooq <64475488+umerfarooq8743@users.noreply.github.com>
2022-12-19 20:24:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function bulb_on() {
+    var bulbimg = document.getElementById("bulbimg");
+    bulbimg.src = "bulb-on.jpg"
+}
+function bulb_off() {
+    var bulbimg = document.getElementById("bulbimg");
+    bulbimg.src = "bulb-off.jpg"
+}
+
+
